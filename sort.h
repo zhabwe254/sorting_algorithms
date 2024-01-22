9bbbde12d0ba4cfb8ba95a29db37f261d75b83ea
zhabwe254 <zhabwe@gmail.com>
2024-01-23 01:28:59 +0300

    A

--- sort.h
+++ sort.h
@@ -25,5 +25,9 @@ void bubble_sort(int *array, size_t size);
 void swap(listint_t *node_a, listint_t *node_b, listint_t **head);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void swap_ints(int *a, int *b);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
 
 #endif /* GH_SORT_H */
