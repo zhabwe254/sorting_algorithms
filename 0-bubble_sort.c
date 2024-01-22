2fca20b08f8c6e2160c4f6727c90c40526b2079d
zhabwe254 <zhabwe@gmail.com>
2024-01-23 01:20:49 +0300

    A

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,28 +1,27 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order
- * @arr: The array to be sorted
- * @len: The size of the array
+ * bubble_sort - Sorts an array of integers in ascending order using
+ *               the Bubble sort algorithm
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
  */
-void bubble_sort(int *arr, size_t len)
+void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
 
-	if (len < 2)
-		return;
-
-	for (i = 0; i < len - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < len - i - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (arr[j] > arr[j + 1])
+			if (array[j] > array[j + 1])
 			{
-				temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
-				print_array(arr, len);
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+
+				print_array(array, size);
 			}
 		}
 	}
--- sort.h
+++ sort.h
@@ -1,17 +1,14 @@
-#ifndef _SORTING_H_
-#define _SORTING_H_
+#ifndef SORT_H
+#define SORT_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
-/** Doubly Linked List Structure */
+#include <stddef.h>
 
 /**
- * struct listint_s - Node in a doubly linked list
+ * struct listint_s - Doubly linked list node
  *
- * @n: Integer value stored in the node
- * @prev: Pointer to the previous node in the list
- * @next: Pointer to the next node in the list
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
@@ -20,57 +17,10 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/** Provided Function Prototypes */
-
-/**
- * print_array - Prints an array of integers
- * @array: The array to be printed
- * @size: Number of elements in the array
- */
+/* Provided print functions */
 void print_array(const int *array, size_t size);
-
-/**
- * print_list - Prints a doubly linked list of integers
- * @list: The list to be printed
- */
 void print_list(const listint_t *list);
 
-/** Task-Specific Function Prototypes */
+void bubble_sort(int *array, size_t size);
 
-void bubble_sort(int *arr, size_t len);
-int len_list(listint_t *head);
-void swap(listint_t *node_a, listint_t *node_b, listint_t **head);
-void insertion_sort_list(listint_t **list);
-void bubble_sort(int *arr, size_t len)
-void swap(int *a, int *b);
-int partition(int *array, int low, int high, size_t size);
-void lomuto_qsort(int *array, int low, int high, size_t size);
-void quick_sort(int *array, size_t size)
-
-void swap_ints(int *a, int *b);
-int lomuto_partition(int *array, size_t size, int left, int right);
-void lomuto_sort(int *array, size_t size, int left, int right);
-void quick_sort(int *array, size_t size);
-
-void selection_sort(int *array, size_t size);
-void quick_recursion(int *array, int left, int right, size_t size);
-int partition(int *array, int left, int right, size_t size);
-void shell_sort(int *array, size_t size);
-void switch_nodes(listint_t **list, listint_t **p);
-void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
-void merge_sort(int *array, size_t size);
-void merge_recursion(int *arr, int *array, size_t left, size_t right);
-void merge_subarray(int *arr, int *array, size_t left,
-                    size_t middle, size_t right);
-void heap_sort(int *array, size_t size);
-void heapify(int *array, size_t s, size_t root, size_t size);
-void radix_sort(int *array, size_t size);
-void count_sort_LSD(int *array, size_t size, size_t lsd);
-void quick_sort_hoare(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
-void bitonic_recursion(int *array, int l, int r, int direction, size_t size);
-void bitonic_merge(int *array, int l, int r, int direction);
-
-#endif /* _GHabwe_SORTING_H_ */
-
+#endif /* GH_SORT_H */
